70539d1b174245721ef66f51b3bfeeefe70bf17e
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-09-08 18:46:03 +0800

    chore: update running message and start script

--- src/index.ts
+++ src/index.ts
@@ -23,5 +23,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`[server] is running on port: ${PORT}`);
+  console.log(`[server] is running on localhost:${PORT}`);
 });
